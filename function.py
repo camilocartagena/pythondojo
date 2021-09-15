b9ea9d1fc649ff4e148acb64359ab8cf1c76dd12
camilo cartagena <camilocartagena@gmail.com>
2021-09-15 09:06:25 -0500

    basic examples

---
+++ function.py
@@ -0,0 +1,8 @@
+def multiplica_por_5(numero):
+    print( str(numero) +  "* 5 = " + str(numero * 5))
+    
+print('Comienzo del programa')    
+multiplica_por_5(7)
+print('Siguiente')
+multiplica_por_5(113)
+print('Fin')
---
+++ startswith.py
@@ -0,0 +1,6 @@
+tmp = "Ingrese"
+print(tmp, '')
+if not tmp.startswith("Ingrese"):
+    print("opcion1")
+else:
+    print("opcion2")
